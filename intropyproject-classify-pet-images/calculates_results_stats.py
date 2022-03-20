608c04683f96e12663fd77be33bc2b8ed887d108
Alan Cruz <cruz.alan@gmail.com>
2022-03-20 14:43:23 -0400

    correcting stat key

--- intropyproject-classify-pet-images/calculates_results_stats.py
+++ intropyproject-classify-pet-images/calculates_results_stats.py
@@ -136,7 +136,7 @@ def calculates_results_stats(results_dic):
         else:
             # Classifier classifies image as NOT a Dog(& pet image isn't a dog)
             # counts number of correct NOT dog clasifications.
-            results_stats_dic['n_correct_dogs'] += 1
+            results_stats_dic['n_correct_notdogs'] += 1
 
 
     # Calculates run statistics (counts & percentages) below that are calculated
